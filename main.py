0e78de4e0beb7e77f3e3b9c7bdf9d3c763368f1a
Kennedy Djeumi <114368365+Kennedydjeumi@users.noreply.github.com>
2023-02-02 21:34:23 +0000

    This code allows a password to contain a maximum of 3 letters, 3 numbers, and 3 symbols. The password must only contain characters from all_chars (letters, numbers, and symbols). If the password violates any of these constraints, an error message is displayed.

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from dolist import tasks
-TASK=tasks
+import re 
+Upper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+symbols=["!", "@","#","$","%","^","&","*","(",")","_","+","-","=","{","}" ,]
+numbers=["1","2","3","4","5","6","7","8","9","0",]
+ 
+all=Upper + lower + symbols 
 
-optional_task=[]
+all_numbers=numbers
 
+password=input("Enter your password:")
 
-def to_do(task):
-     optional_task.append(task)
+letter_count = len(re.findall(r"[a-zA-Z]", password))
 
-for task in TASK:
-    to_do(task)
+number_count = len(re.findall(r"\d", password))
 
-def do():
-    print("To do list: ")
-    for i, task in enumerate(tasks):
-        print(i + 1, task)
+symbol_count = len(re.findall(r"[!@#\$%^&*()_\-+={}|;:<,>.?/]", password))
 
-if optional_task:
-    do()
+if letter_count > 3:
+    print("Password should not contain more than 3 letters.")
+
+elif number_count > 3:
+
+  print("Password should not contain more than 3 numbers.")
+
+elif symbol_count > 3:
+  
+  print("Password should not contain more than 3 symbols.")
+
+elif any(char not in all_chars for char in password):
+
+  print("Password should only contain letters, numbers, and symbols.")
+
 else:
-    print("The task list is empty")
+    print("Password accepted.")
